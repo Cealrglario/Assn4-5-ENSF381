ca3d35648f33c0963b95518b36c1e8680da5ed4f
Cealrglario <kirstencarl.elgario@ucalgary.ca>
2024-03-21 14:13:07 -0600

    Added login page with a "not yet implemented" message

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
 
 function Login() {
+    return (
+        <div>
+            <Header />
+            <br />
+            <p style={{textAlign: 'center'}}>Not yet implemented</p>
+            <Footer />
+        </div>
+    )
 }
 
 export default Login;
